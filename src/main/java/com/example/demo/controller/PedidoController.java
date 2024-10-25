af58b0d2e8b3041e45a55346e1008c5a14bcf038
zchanway <146023170+zchanway@users.noreply.github.com>
2024-10-25 02:39:29 -0500

    PedidoController

--- src/main/java/com/example/demo/controller/PedidoController.java
+++ src/main/java/com/example/demo/controller/PedidoController.java
@@ -1,4 +1,28 @@
 package com.example.demo.controller;
 
+import com.example.demo.model.Pedido;
+import com.example.demo.service.IPedidoService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping(path = "api/v1/pedidos")
 public class PedidoController {
+
+    private final IPedidoService pedidoService;
+
+    @GetMapping("/customer/{clienteId}/status/{estadoEnvio}")
+    public List<Pedido> buscarPedidosPorClienteYEstadoLogistica(@PathVariable Integer clienteId, @PathVariable String estadoEnvio) {
+        return pedidoService.buscarPedidosPorClienteYEstadoLogistica(clienteId, estadoEnvio);
+    }
+    @GetMapping("/cliente/{clienteId}")
+    public List<Pedido> getPedidosByClienteId(@PathVariable Integer clienteId) {
+        return pedidoService.findByClienteId(clienteId);
+    }
 }
