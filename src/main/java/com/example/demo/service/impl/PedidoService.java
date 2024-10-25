f066d54fa076f58c0d76ebc7724758e11f3bd710
zchanway <146023170+zchanway@users.noreply.github.com>
2024-10-25 02:37:53 -0500

    PedidoService

--- src/main/java/com/example/demo/service/impl/PedidoService.java
+++ src/main/java/com/example/demo/service/impl/PedidoService.java
@@ -1,4 +1,30 @@
 package com.example.demo.service.impl;
 
-public class PedidoService {
+import com.example.demo.model.Pedido;
+import com.example.demo.repository.ClienteRepository;
+import com.example.demo.repository.PedidoRepository;
+import com.example.demo.service.IPedidoService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Service
+public class PedidoService implements IPedidoService {
+    private final PedidoRepository pedidoRepository;
+    private final ClienteRepository clienteRepository;
+    @Override
+    public List<Pedido> buscarPedidosPorClienteYEstadoLogistica(Integer clienteId, String estadoEnvio) {
+        return pedidoRepository.buscarPedidosPorClienteYEstadoLogistica(clienteId, estadoEnvio);
+    }
+
+
+    @Override
+    public List<Pedido> findByClienteId(Integer cliente_id) {
+        return pedidoRepository.findByClienteId(cliente_id);
+    }
+
+
+
 }
