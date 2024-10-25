79d305f7b0b1cc3833caf352cff4e0a1add2a605
Aladan Lazo Espinoza <faridlazo1921@gmail.com>
2024-10-25 02:28:57 -0500

    implements ILogisticaService, LogisticaService

--- src/main/java/com/example/demo/service/ILogisticaService.java
+++ src/main/java/com/example/demo/service/ILogisticaService.java
@@ -1,4 +1,8 @@
 package com.example.demo.service;
 
+import com.example.demo.model.Logistica;
+
 public interface ILogisticaService {
+
+    Logistica actualizarEstadoLogistica(String estadoEnvio, String idlogistica);
 }
--- src/main/java/com/example/demo/service/impl/LogisticaService.java
+++ src/main/java/com/example/demo/service/impl/LogisticaService.java
@@ -1,4 +1,22 @@
 package com.example.demo.service.impl;
 
-public class LogisticaService {
+import com.example.demo.model.Logistica;
+import com.example.demo.repository.LogisticaRepository;
+import com.example.demo.service.ILogisticaService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class LogisticaService implements ILogisticaService {
+    private LogisticaRepository logisticaRepository;
+
+    @Override
+    public Logistica actualizarEstadoLogistica(String estadoEnvio, String idlogistica) {
+        Logistica logistica = logisticaRepository.findById(Integer.valueOf(idlogistica))
+                .orElseThrow(() -> new RuntimeException("Logística no encontrada"));
+
+        logistica.setEstadoEnvio(estadoEnvio);
+        return logisticaRepository.save(logistica);
+    }
 }
