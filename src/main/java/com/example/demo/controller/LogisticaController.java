24f4ae3ea881e7498b6e4cd9f50e5ee77e1d16ee
zchanway <146023170+zchanway@users.noreply.github.com>
2024-10-25 02:34:29 -0500

    Logistica controller

--- src/main/java/com/example/demo/controller/LogisticaController.java
+++ src/main/java/com/example/demo/controller/LogisticaController.java
@@ -1,4 +1,22 @@
 package com.example.demo.controller;
 
+import com.example.demo.model.Logistica;
+import com.example.demo.service.ILogisticaService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping(path = "api/v1/logistica")
 public class LogisticaController {
+    private final ILogisticaService logisticaService;
+
+    @PutMapping("/actualizarEstado/{id}")
+    public ResponseEntity<Logistica> actualizarEstado(@PathVariable Integer id, @RequestParam String estadoActivo) {
+        Logistica logisticaActualizada = logisticaService.actualizarEstadoLogistica(String.valueOf(id), estadoActivo);
+        return new ResponseEntity<>(logisticaActualizada, HttpStatus.OK);
+    }
+
 }
