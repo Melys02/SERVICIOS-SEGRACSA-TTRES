cb368e600e01960e4711dd09627c27523a9e7780
zchanway <146023170+zchanway@users.noreply.github.com>
2024-10-25 02:36:20 -0500

    IPedidoService

--- src/main/java/com/example/demo/service/IPedidoService.java
+++ src/main/java/com/example/demo/service/IPedidoService.java
@@ -1,4 +1,13 @@
 package com.example.demo.service;
 
+import com.example.demo.model.Pedido;
+
+import java.util.List;
+
 public interface IPedidoService {
+
+    List<Pedido> buscarPedidosPorClienteYEstadoLogistica(Integer clienteId, String estadoEnvio);
+    List<Pedido> findByClienteId(Integer cliente_id);
+
+
 }
